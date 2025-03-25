2ab2e164684550c3749180ca8ca17f6d7ada23c7
Abhinav Kaushal <abhinavkaushalofficial@gmail.com>
2025-03-25 23:18:06 +0530

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 load_dotenv()
 
-IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
-FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
-TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+IATA_ENDPOINT = "*************"
+FLIGHT_ENDPOINT = "*************"
+TOKEN_ENDPOINT = "*************"
 
 
 class FlightSearch:
